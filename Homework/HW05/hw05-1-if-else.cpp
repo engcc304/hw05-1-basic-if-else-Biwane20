c50a81524f9f3c937c372d123dd39b5a0fab8cbc
Biwane20 <biwbew20@gmail.com>
2023-08-16 23:33:49 +0700

    Hw5.1

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -1,6 +1,6 @@
 /*
     รับตัวอักขระมาทั้งหมด 4 ค่า แล้วนำมาแสดงผลย้อนกลับ เช่น กรอกค่า L M X Y ต้องแสดงเป็น Y X M L เป็นต้น
-    
+
     Test case:
         L M Y K
     Output:
@@ -11,3 +11,43 @@
     Output:
         Result: D C B A
 */
+#include <stdio.h>
+int main()
+{
+    int a = 0, b = 0, c = 0;
+    printf("Input[1] ");
+    scanf("%d", &a);
+    printf("Input[2] ");
+    scanf("%d", &b);
+    printf("Input[3] ");
+    scanf("%d", &c);
+
+    if (a > b && a > c)
+    {
+        printf("%d ", a);
+        if (b > c)
+            printf("%d %d ", b, c);
+        else
+            printf("%d %d ", c, b);
+    }
+
+    else if (b > a && b > c)
+    {
+        printf("%d ", b);
+        if (a > c)
+            printf("%d %d ", a, c);
+        else
+            printf("%d %d ", c, a);
+    }
+
+    else if (c > b && c > a)
+    {
+        printf("%d ", c);
+        if (b > a)
+            printf("%d %d ", b, a);
+        else
+            printf("%d %d ", a, b);
+    }
+
+    return 0;
+}
